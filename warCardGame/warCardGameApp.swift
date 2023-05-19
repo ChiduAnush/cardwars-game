ba6fddeea289187c14e44b4d32b1459655506656
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 15:58:58 +0530

    Initial Commit

---
+++ warCardGame/warCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warCardGameApp.swift
+//  warCardGame
+//
+//  Created by Chidu Anush on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct warCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
