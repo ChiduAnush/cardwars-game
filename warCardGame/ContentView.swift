a74c646a2b33803f4cb10a6cf9b7c3e9bf8a0f49
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 16:26:59 +0530

    Make the base UI

--- warCardGame/ContentView.swift
+++ warCardGame/ContentView.swift
@@ -10,7 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-    
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                
+                Image("button")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+                
+                
+            }
+        }
         
         
     }
